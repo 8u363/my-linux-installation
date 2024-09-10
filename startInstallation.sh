97decf7763298525a81c5d9389a84a0b207b9b85
8u3631984 <74074904+8u3631984@users.noreply.github.com>
2024-09-10 15:09:25 +0200

    3 install yay (#4)
    * add paru installation script
    
    * add git function
    
    * add menu
    
    * update output

--- startInstallation.sh
+++ startInstallation.sh
@@ -6,9 +6,21 @@ clear
 source library/commons.sh
 # -----------------------------------------------------
 
-printInfo "update"
+# -----------------------------------------------------
+printInfo "update packages"
 sudo pacman -Syu
 
 printInfo "install required packages"
 installPackagesWithPackman "figlet" "gum"
+# -----------------------------------------------------
 
+# -----------------------------------------------------
+printBanner "My Linux Arch Installation"
+# -----------------------------------------------------
+
+installAURHelper="install AUR helper"
+selected=$(gum choose "$installAURHelper" --selected="$installAURHelper" --no-limit)
+
+if [[ $selected = *"$installAURHelper"* ]]; then
+    source scripts/installAURHelper.sh
+fi
