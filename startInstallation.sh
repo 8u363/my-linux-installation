b29882b900e93606b811039b360f7d349ea74ade
Sebastian Büge <sebastian_buege@gmx.de>
2024-09-10 15:54:19 +0200

    add install software script (#10)

--- startInstallation.sh
+++ startInstallation.sh
@@ -20,8 +20,9 @@ printBanner "My Linux Arch Installation"
 
 configPacman="config pacman"
 installAURHelper="install AUR helper"
+installSoftware="install software"
 copyWallpaper="copy Wallpaper"
-selected=$(gum choose "$configPacman" "$installAURHelper" "$copyWallpaper" --selected="$configPacman","$installAURHelper","$copyWallpaper" --no-limit)
+selected=$(gum choose "$configPacman" "$installAURHelper" "$installSoftware" "$copyWallpaper" --selected="$configPacman","$installAURHelper","$installSoftware","$copyWallpaper" --no-limit)
 
 if [[ $selected = *"$configPacman"* ]]; then
     source scripts/configPacman.sh
@@ -31,6 +32,9 @@ if [[ $selected = *"$installAURHelper"* ]]; then
     source scripts/installAURHelper.sh
 fi
 
+if [[ $selected = *"$installSoftware"* ]]; then
+    source scripts/installSoftware.sh
+fi
 
 if [[ $selected = *"$copyWallpaper"* ]]; then
     source scripts/copyWallpaperRepository.sh
