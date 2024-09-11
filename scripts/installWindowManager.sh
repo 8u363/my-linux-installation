f7c612e57bf704d4a2f5dce52c0c8d7d393db632
8u3631984 <8u3631984@gmail.com>
2024-09-11 11:09:52 +0200

    add winod manager install script

---
+++ scripts/installWindowManager.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+i3Packages=(
+    "alacritty"                         # terminal emulator 
+    "autotiling"                        # automatic switching horizontal / vertical window split orientation
+    "feh"                               # image viewer, used to set the background image
+    "i3-wm"                             # i3 display manager
+    "dmenu"                             # dynamic menu for X
+    "picom"                             # standalone compositor for Xorg
+    "polybar"                           # status bar 
+)
+
+swayPackages=(
+    "alacritty"                         # terminal emulator 
+    "feh"                               # image viewer, used to set the background image
+    "picom"                             # standalone compositor for Xorg
+    "sway"                              # compositor for Wayland
+    "wofi"                              # command launcher
+    "waybar"                            # status bar for Sway 
+    "wlogout"                           # logout menu for wayland
+    "waypaper"                          # wallpaper setter for Wayland 
+)
+
+printInfo "which window manager should be installed?"
+
+services=$(gum choose "i3" "sway" --no-limit)
+
+if [[ $services = *"i3"* ]]; then
+    printInfo "install i3 packages"
+    installPackagesWithPackman "${i3Packages[@]}"
+fi
+
+if [[ $services = *"sway"* ]]; then
+    printInfo "install i3 packages"
+    installPackagesWithPackman "${swayPackages[@]}"
+fi
