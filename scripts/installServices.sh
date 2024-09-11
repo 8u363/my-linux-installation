75d287eaee7679bc20536b8a2ce8d6de524e8550
8u3631984 <8u3631984@gmail.com>
2024-09-11 09:31:59 +0200

    print info

--- scripts/installServices.sh
+++ scripts/installServices.sh
@@ -7,27 +7,32 @@ printInfo "which services should be installed?"
 services=$(gum choose "cups" "docker" "ssh" "ntp" "lightdm" --no-limit)
 
 if [[ $services = *"cups"* ]]; then
+    printInfo "install and enable cups service"
     installPackagesWithPackman "cups" "cups-pdf"
     sudo systemctl enable cups.service
 fi
 
 if [[ $services = *"docker"* ]]; then
+    printInfo "install and enable docker service"
     installPackagesWithPackman "docker" "docker-compose"
     sudo systemctl enable docker.service
 fi
 
 if [[ $services = *"ssh"* ]]; then
+    printInfo "install and enable ssh service"
     installPackagesWithPackman "openssh"
     sudo systemctl enable sshd.service
 fi
 
 if [[ $services = *"ntp"* ]]; then
+    printInfo "install and enable ntp service"
     installPackagesWithPackman "ntp"
     sudo systemctl enable ntpd.service
     sudo ntpdate pool.ntp.org
 fi
 
 if [[ $services = *"lightdm"* ]]; then
+    printInfo "install and enable lightdm service"
     installPackagesWithPackman "lightdm" "lightdm-gtk-greeter"
     sudo systemctl enable lightdm.service
 fi
