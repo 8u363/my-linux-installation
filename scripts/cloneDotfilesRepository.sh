cd57d1f53f0b5775aba3d7065ea8ab8a1255cbe4
Sebastian Büge <sebastian_buege@gmx.de>
2024-09-11 11:30:59 +0200

    clone copy dotfile repository and refactor (#21)
    * remove instal aur script
    
    * rename copy wallpaper script
    
    * add clone dotfile option
    
    * add script to clone dotfiles

---
+++ scripts/cloneDotfilesRepository.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+printScriptHeader "dotfiles"
+# -----------------------------------------------------
+
+WALLPAPER_GIT="https://github.com/8u3631984/my-dotfiles.git"
+WALLPAPER_DIR="/home/$USER/git/my-dotfiles"
+
+cloneGitReposiotry $WALLPAPER_GIT $WALLPAPER_DIR
