ed70b5ad8384ef59cb554901903ce356b6cef9a3
Sebastian Büge <sebastian_buege@gmx.de>
2024-09-11 15:15:25 +0200

    install stow (#28)
    Co-authored-by: 8u3631984 <8u3631984@gmail.com>

--- scripts/cloneDotfilesRepository.sh
+++ scripts/cloneDotfilesRepository.sh
@@ -5,5 +5,8 @@ printScriptHeader "dotfiles"
 
 WALLPAPER_GIT="https://github.com/8u3631984/my-dotfiles.git"
 WALLPAPER_DIR="/home/$USER/git/my-dotfiles"
+CONFIG_DIRECTORY="/home/$USER/.config"
 
 cloneGitReposiotry $WALLPAPER_GIT $WALLPAPER_DIR
+
+installPackagesWithPackman "stow"
