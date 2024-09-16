ede965bbb3c12304a2a2cee703ec8fa8b52c4175
Sebastian Büge <sebastian_buege@gmx.de>
2024-09-16 09:04:09 +0200

    remove i3 and sway (#35)
    * remoive i3 and sway use qtile instead

--- scripts/installWindowManager.sh
+++ scripts/installWindowManager.sh
@@ -8,48 +8,13 @@ qtilePackages=(
     "qtile"                             # qtile display manager
 )
 
-i3Packages=(
-    "alacritty"                         # terminal emulator 
-    "autotiling"                        # automatic switching horizontal / vertical window split orientation
-    "feh"                               # image viewer, used to set the background image
-    "i3-wm"                             # i3 display manager
-    "dmenu"                             # dynamic menu for X
-    "picom"                             # standalone compositor for Xorg
-    "polybar"                           # status bar 
-)
-
-swayPackages=(
-    "alacritty"                         # terminal emulator 
-    "feh"                               # image viewer, used to set the background image
-    "picom"                             # standalone compositor for Xorg
-    "sway"                              # compositor for Wayland
-    "wofi"                              # command launcher
-    "waybar"                            # status bar for Sway 
-)
-
-swayAURPackages=(
-    "wlogout"                           # logout menu for wayland
-    "waypaper"                          # wallpaper setter for Wayland 
-)
-
 printScriptHeader "window manager"
 printInfo "which window manager should be installed?"
 # -----------------------------------------------------
 
-services=$(gum choose "qtile" "sway" --selected="qtile","i3","sway" --no-limit)
+services=$(gum choose "qtile" --selected="qtile" --no-limit)
 
 if [[ $services = *"qtile"* ]]; then
     printInfo "install qtile packages"
     installPackagesWithPackman "${qtilePackages[@]}"
-fi
-
-if [[ $services = *"i3"* ]]; then
-    printInfo "install i3 packages"
-    installPackagesWithPackman "${i3Packages[@]}"
-fi
-
-if [[ $services = *"sway"* ]]; then
-    printInfo "install i3 packages"
-    installPackagesWithPackman "${swayPackages[@]}"
-    installPackagesWithParu "${swayAURPackages[@]}"
 fi
