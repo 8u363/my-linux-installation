3b023292c551023bb93852d54439a6e1cb3299b1
8u3631984 <8u3631984@gmail.com>
2024-10-15 21:18:53 +0200

    use installSoftware function

--- scripts/services/installServices.sh
+++ scripts/services/installServices.sh
@@ -4,22 +4,22 @@
 printScriptHeader "services"
 
 printInfo "install and enable cups service"
-installPackagesWithPackman "cups" "cups-pdf"
+installSoftware "cups" "cups-pdf"
 sudo systemctl enable cups.service
 
 printInfo "install and enable docker service"
-installPackagesWithPackman "docker" "docker-compose"
+installSoftware "docker" "docker-compose"
 sudo systemctl enable docker.service
 
 printInfo "install and enable ssh service"
-installPackagesWithPackman "openssh"
+installSoftware "openssh"
 sudo systemctl enable sshd.service
 
 printInfo "install and enable ntp service"
-installPackagesWithPackman "ntp"
+installSoftware "ntp"
 sudo systemctl enable ntpd.service
 sudo ntpdate pool.ntp.org
 
 printInfo "install and enable lightdm service"
-installPackagesWithPackman "lightdm" "lightdm-gtk-greeter"
+installSoftware "lightdm" "lightdm-gtk-greeter"
 sudo systemctl enable lightdm.service
