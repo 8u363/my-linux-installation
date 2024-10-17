27519d85e98802696b7301fab7b339cedebed87e
Sebastian Büge <sebastian_buege@gmx.de>
2024-10-17 14:26:50 +0200

    39 refactor (#40)
    * rename library to commons package
    
    * rename common function
    
    * split info output
    
    * print banner
    
    * add options
    
    * add variabel options
    
    * move scripts in subfolders
    
    * install paru
    
    * update output
    
    * move common script
    
    * install software
    
    * use option varaible
    
    * use installSoftware function
    
    * use installSoftware function
    
    * add wallpaper option
    
    * clone dotfile repo
    
    * add option
    
    * add cleanup option
    
    * run install script to dotfiles
    
    * execute stow
    
    * install nitrogen
    
    * add additional software option
    
    * add software installation option
    
    * update logging
    
    * add logging in copy dotfiles
    
    * add cleanup option
    
    * fix typo
    
    * fix typo
    
    ---------
    
    Co-authored-by: 8u3631984 <8u3631984@gmail.com>

--- scripts/configPacman.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-printScriptHeader "pacman"
-# -----------------------------------------------------
-
-printInfo "copy pacman configuration"
-sudo cp scripts/pacman.conf /etc/pacman.conf
