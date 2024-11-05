0a0b12815924999bae1d101f6bdd4ecbc200057b
Sebastian Büge <sebastian_buege@gmx.de>
2024-11-05 11:00:07 +0100

    chnage dotfiles directory

--- scripts/dotfiles/installDotfiles.sh
+++ scripts/dotfiles/installDotfiles.sh
@@ -3,7 +3,7 @@
 # -----------------------------------------------------
 printScriptHeader "dotfiles"
 
-DOTFILES_DIR="../../dotfiles"
+DOTFILES_DIR="../../dotfiles/"
 CONFIG_DIRECTORY="/home/$USER/.config"
 
 stow config --adopt --dir $DOTFILES_DIR --target $CONFIG_DIRECTORY
