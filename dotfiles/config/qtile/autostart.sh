d6322cc2fe51fdb9fa709fcaa83b4630eca5be43
Sebastian Büge <sebastian_buege@gmx.de>
2024-11-05 15:15:30 +0100

    change script permission

--- dotfiles/config/qtile/autostart.sh
+++ dotfiles/config/qtile/autostart.sh
@@ -2,7 +2,7 @@
 
 xrandr --output Virtual1 --primary --mode 1600x900 --pos 0x0 --rotate normal
 
-~/.config/qtile/scripts/setDefaultWallpaper.sh
+~/.config/qtile/scripts/setDefaultWallpaper.sh &
 
 dunst &
 blueman-applet &
