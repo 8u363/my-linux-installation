c19f317cdc588322254ee46eb4e8a16e159f2aac
Sebastian Büge <sebastian_buege@gmx.de>
2024-11-05 12:04:53 +0100

    create config directory

--- scripts/dotfiles/installDotfiles.sh
+++ scripts/dotfiles/installDotfiles.sh
@@ -6,4 +6,5 @@ printScriptHeader "dotfiles"
 DOTFILES_DIR="../../dotfiles/"
 CONFIG_DIRECTORY="/home/$USER/.config"
 
+mkdir -p $CONFIG_DIRECTORY
 stow config --adopt --dir $DOTFILES_DIR --target $CONFIG_DIRECTORY
