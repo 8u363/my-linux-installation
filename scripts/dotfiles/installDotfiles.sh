51b81cbbc62eb8fe8b8224188a93441b34fef2d1
Sebastian Büge <sebastian_buege@gmx.de>
2024-11-06 21:11:02 +0100

    update zshenv path

--- scripts/dotfiles/installDotfiles.sh
+++ scripts/dotfiles/installDotfiles.sh
@@ -9,5 +9,5 @@ CONFIG_DIRECTORY="/home/$USER/.config"
 mkdir -p $CONFIG_DIRECTORY
 stow config --adopt --dir $DOTFILES_DIR --target $CONFIG_DIRECTORY
 
-cp $DOTFILES_DIR/zshenv ~/.zshenv
+cp $DOTFILES_DIR/zshenv "/home/$USER/.zshenv"
 sudo chsh $USER -s $(which zsh)
